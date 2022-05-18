b70e77c3c5320d1599a5dda2dbfd77b724122ff6
srilakshmi <lakshmi1996.eee@gmail.com>
2022-05-18 12:10:24 +0530

    firstgit

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,19 @@
+package org.emp;
+
+public class Employee {
+	private void empName() {
+		System.out.println("empName is srilakshmi");
+
+	}
+	private void empId() {
+	  System.out.println("emp id 12434");
+
+	}
+	
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.empName();
+		e.empId();
+	}
+
+}
